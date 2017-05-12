ec04d9f6a4ab82ef49f93913b93bc73d3e537285
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2017-05-12 01:06:41 -0700

    set TF Login level

--- train.sh
+++ train.sh
@@ -10,5 +10,7 @@ baseDir=$(cd `dirname "$0"`;pwd)
 # main 
 [ -z "${BASH_SOURCE[0]}" -o "${BASH_SOURCE[0]}" = "$0" ] || return
 source /root/venv-py3/bin/activate
+# http://stackoverflow.com/questions/35911252/disable-tensorflow-debugging-information
+export TF_CPP_MIN_LOG_LEVEL=3
 cd $baseDir
 python neural_conversation_model.py --train_dir data/ --en_vocab_size 60000 --size 512 --data_path data/train.tsv --dev_data data/vali.tsv  --vocab_path data/60k_vocan.zh --attention
